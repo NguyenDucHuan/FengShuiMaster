0c1becbfff2397e8c0b077fb3e65a897363ac4b1
NguyenDucHuan <NguyenDucHuan14012003@gmail.com>
2024-11-02 00:10:37 +0700

    fix3

--- src/controllers/destintyController.js
+++ src/controllers/destintyController.js
@@ -10,7 +10,6 @@ const getAllDestinies = async (req, res) => {
 }
 const createDestiny = async (req, res) => {
     try {
-
         const { name } = req.body;
         let data = await destinyService.createDestinie(name)
         return res.status(200).json(data);
